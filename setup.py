6620c3a7b3a3ef5721bc572d34b550792c0c4ad5
Brewster Malevich <bmalevich@rhg.com>
2019-10-31 10:34:08 -0700

    Add black and precommit hooks

--- setup.py
+++ setup.py
@@ -1,52 +1,51 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'numpy',
-    'pyyaml',
+    "numpy",
+    "pyyaml",
 ]
 
 setup_requirements = [
-    'pytest-runner',
-    # TODO(brews): put setup requirements (distutils extensions, etc.) here
+    "pytest-runner",
 ]
 
 test_requirements = [
-    'flake8',
-    'pytest',
+    "flake8",
+    "pytest",
 ]
 
 setup(
-    name='dalia',
-    version='0.0.1a1',
+    name="dalia",
+    version="0.0.1a1",
     description="Goddess of fate - giver and taker of goods",
-    long_description=readme + '\n\n' + history,
-    long_description_content_type='text/x-rst',
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
     author="Brewster Malevich",
-    author_email='bmalevich@rhg.com',
-    url='https://github.com/brews/dalia',
-    packages=find_packages(include=['dalia']),
+    author_email="bmalevich@rhg.com",
+    url="https://github.com/brews/dalia",
+    packages=find_packages(include=["dalia"]),
     include_package_data=True,
     install_requires=requirements,
     zip_safe=False,
-    keywords='dalia',
+    keywords="dalia",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Sociology',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Sociology",
     ],
     tests_require=test_requirements,
     setup_requires=setup_requirements,
